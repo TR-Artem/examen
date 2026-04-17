719697314a3036a5f22dc3bbf680930ba9ea08ab
TR-Artem Nesq <your_email@example.com>
2026-04-17 13:25:36 +0300

    готовое доработанное решение

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from app.core.database import Base, engine
 from app.api.routers import auth, tasks, admin
-from app.db.base import Base
-from app.db.session import engine
 
-app = FastAPI()
+app = FastAPI(
+    title="Task Tracker API",
+    version="1.0.0"
+)
 
-Base.metadata.create_all(bind=engine)
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+    print("✅ База данных создана")
 
 app.include_router(auth.router)
 app.include_router(tasks.router)
 app.include_router(admin.router)
+
+@app.get("/")
+def root():
+    return {"message": "Task Tracker API is running"}
